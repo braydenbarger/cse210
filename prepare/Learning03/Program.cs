4cca8f3c4eb6c4bca43f1bdc1bc17e083e2cad38
braydenbarger <bar21065@byui.edu>
2025-02-05 19:28:32 -0700

    fraction

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,28 @@
 using System;
 
-class Program
+namespace Learning03
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Learning03 World!");
+        static void Main(string[] args)
+        {
+            Fraction fraction1 = new Fraction();
+            Fraction fraction2 = new Fraction(6);
+            Fraction fraction3 = new Fraction(6, 7);
+
+            Console.WriteLine(fraction1.GetFractionString());
+            Console.WriteLine(fraction1.GetDecimalValue());
+
+            Console.WriteLine(fraction2.GetFractionString());
+            Console.WriteLine(fraction2.GetDecimalValue());
+
+            Console.WriteLine(fraction3.GetFractionString());
+            Console.WriteLine(fraction3.GetDecimalValue());
+
+            fraction1.SetTop(5);
+            fraction1.SetBottom(2);
+            Console.WriteLine(fraction1.GetFractionString());
+            Console.WriteLine(fraction1.GetDecimalValue());
+        }
     }
 }
